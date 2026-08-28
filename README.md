80377754dc7dbe432660d47d9349b76c40ec49ad
Cristián Rojas <cristian.rojas@msn.com>
2026-08-28 16:29:03 -0400

    Agregar descuento del 10 por ciento

---
+++ README.md
@@ -0,0 +1,14 @@
+#Calculadora de precios
+
+Actividad práctica de Github
+
+## Integrantes
+- Nombre estudiante 1
+- Nombre estudiante 2
+- Nombre estudiante 3
+
+## Tecnologías
+- Python
+- Git
+- Github
+- Github Actions
--- precios.py
+++ precios.py
@@ -1,8 +1,14 @@
 def calcular_total (precio, cantidad):
     return precio * cantidad
 
+def aplicar_descuento(total, porcentaje):
+    descuento = total * porcentaje /100
+    return total - descuento
+
 def mostrar_total(precio, cantidad):
     total = calcular_total(precio, cantidad)
+    total = aplicar_descuento(total, 10)
+    
     print(f"Total compra : ${total}")
 
 mostrar_total(5000, 3)
